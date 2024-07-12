aa70b1260283ffd4f5a3dc6d66883cbe3ed507a3
Samiksha <mohit3rke@gmail.com>
2024-07-12 21:32:11 +0530

    Solution using react memo

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,55 @@
-import { useState } from 'react'
-
-// function App() {
-//   const [firstTitle,setFirstTitle]=useState("my name is abc");
-
-//   function changeTitle(){
-//     setFirstTitle("my name is "+Math.random())
-//   }
-
-//   return (
-//     <>
-//       <button onClick={changeTitle}>Click me to change the title</button>
-//       <Header title={firstTitle}></Header>
-//       <Header title="xyz"></Header>
-//     </>
-//   )
-// }
+import { memo, useState } from 'react'
 
 function App() {
-
-  return (
-    <>
-      <HeaderWithButton></HeaderWithButton>
-      <Header title="xyz"></Header>
-    </>
-  )
-}
-
-
-function HeaderWithButton(){
   const [firstTitle,setFirstTitle]=useState("my name is abc");
 
   function changeTitle(){
     setFirstTitle("my name is "+Math.random())
   }
 
-  return <>
+  return (
+    <>
       <button onClick={changeTitle}>Click me to change the title</button>
       <Header title={firstTitle}></Header>
-  </>
+      <Header title="xyz"></Header>
+    </>
+  )
 }
 
-function Header({title}){
+// function App() {
+
+//   return (
+//     <>
+//       <HeaderWithButton></HeaderWithButton>
+//       <Header title="xyz"></Header>
+//     </>
+//   )
+// }
+
+
+// function HeaderWithButton(){
+//   const [firstTitle,setFirstTitle]=useState("my name is abc");
+
+//   function changeTitle(){
+//     setFirstTitle("my name is "+Math.random())
+//   }
+
+//   return <>
+//       <button onClick={changeTitle}>Click me to change the title</button>
+//       <Header title={firstTitle}></Header>
+//   </>
+// }
+
+// function Header({title}){
+//   return <div>
+//     {title}
+//   </div>
+// }
+
+const Header=memo(function({title}){
   return <div>
     {title}
   </div>
-}
+})
 
 export default App
