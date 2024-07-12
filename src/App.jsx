b984cfbd06425f93874da30e64cc5315a4cdd30e
Samiksha <mohit3rke@gmail.com>
2024-07-12 21:27:45 +0530

    Soltuion to re-render

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
 import { useState } from 'react'
 
+// function App() {
+//   const [firstTitle,setFirstTitle]=useState("my name is abc");
+
+//   function changeTitle(){
+//     setFirstTitle("my name is "+Math.random())
+//   }
+
+//   return (
+//     <>
+//       <button onClick={changeTitle}>Click me to change the title</button>
+//       <Header title={firstTitle}></Header>
+//       <Header title="xyz"></Header>
+//     </>
+//   )
+// }
+
 function App() {
+
+  return (
+    <>
+      <HeaderWithButton></HeaderWithButton>
+      <Header title="xyz"></Header>
+    </>
+  )
+}
+
+
+function HeaderWithButton(){
   const [firstTitle,setFirstTitle]=useState("my name is abc");
 
   function changeTitle(){
     setFirstTitle("my name is "+Math.random())
   }
 
-  return (
-    <>
+  return <>
       <button onClick={changeTitle}>Click me to change the title</button>
       <Header title={firstTitle}></Header>
-      <Header title="xyz"></Header>
-    </>
-  )
+  </>
 }
 
 function Header({title}){
